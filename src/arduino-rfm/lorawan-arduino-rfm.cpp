7115ac115a4d7ff8b98be3131b105796f33ccff4
Eduardo <werowero.edy@gmail.com>
2020-02-24 01:17:05 -0600

    add prefix function

--- src/arduino-rfm/lorawan-arduino-rfm.cpp
+++ src/arduino-rfm/lorawan-arduino-rfm.cpp
@@ -31,7 +31,10 @@
 #include "lorawan-arduino-rfm.h"
 #include "Conversions.h"
 
-LoRaWANClass::LoRaWANClass()
+#define ISR_PREFIX
+
+LoRaWANClass::LoRaWANClass():
+  _onReceive(NULL)
 {
 
 }
@@ -253,9 +256,6 @@ void LoRaWANClass::sendUplink(char *data, unsigned int len, unsigned char confir
     }
 
     LoRa_Settings.Confirm = (confirm == 0) ? 0 : 1;
-
-    //Set new command 
-    //RFM_Command_Status = NEW_RFM_COMMAND;
     
     Buffer_Tx.Counter = len;
     memcpy(Buffer_Tx.Data,data,len);
@@ -370,8 +370,31 @@ void LoRaWANClass::update(void)
           Rx_Status = NEW_RX;
         }        
       }
-
 }
+
+void LoRaWANClass::onReceive(void(*callback)(int))
+{
+  _onReceive = callback;
+  LORA_Set_Interrupt();
+  if (callback) {
+    attachInterrupt(digitalPinToInterrupt(RFM_pins.DIO0), LoRaWANClass::onDio0Rise, RISING);
+  } else {
+    detachInterrupt(digitalPinToInterrupt(RFM_pins.DIO0));
+  }
+}
+
+
+void LoRaWANClass::ISR_handler(void)
+{
+    LORA_Get_Data(&Buffer_Rx, &Session_Data, &OTAA_Data, &Message_Rx, &LoRa_Settings);
+}
+
+
+ISR_PREFIX void LoRaWANClass::onDio0Rise()
+{
+  lora.ISR_handler();
+}
+
 
 void LoRaWANClass::randomChannel()
 {
@@ -397,8 +420,6 @@ unsigned int LoRaWANClass::getFrameCounter() {
 void LoRaWANClass::setFrameCounter(unsigned int FrameCounter) {
     Frame_Counter_Tx = FrameCounter;
 }
-
-
 
 // define lora objet 
 LoRaWANClass lora;
